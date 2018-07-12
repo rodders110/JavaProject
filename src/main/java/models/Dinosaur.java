5943ed73ac77459b4a98ab5420fd56a0af9f48d0
Campbell Miller <campbellmiller4e@gmail.com>
2018-07-12 14:40:28 +0100

    writes dinosaur abstract class

--- src/main/java/models/Dinosaur.java
+++ src/main/java/models/Dinosaur.java
@@ -1,4 +1,40 @@
 package models;
 
-public class Dinosaur {
+public abstract class Dinosaur {
+
+    private int id;
+    private Species species;
+    private Boolean inCaptivity;
+
+    public Dinosaur(Species species, Boolean inCaptivity) {
+        this.species = species;
+        this.inCaptivity = inCaptivity;
+    }
+
+    public Dinosaur() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public Boolean getInCaptivity() {
+        return inCaptivity;
+    }
+
+    public void setInCaptivity(Boolean inCaptivity) {
+        this.inCaptivity = inCaptivity;
+    }
 }
--- src/main/java/models/Herbivore.java
+++ src/main/java/models/Herbivore.java
@@ -1,4 +1,11 @@
 package models;
 
-public class Herbivore {
+public class Herbivore extends Dinosaur {
+
+    public Herbivore(Species species, Boolean inCaptivity) {
+        super(species, inCaptivity);
+    }
+
+    public Herbivore() {
+    }
 }
