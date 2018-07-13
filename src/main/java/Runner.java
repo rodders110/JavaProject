45593165ee080140186be74135e0a78beab362f9
Campbell Miller <campbellmiller4e@gmail.com>
2018-07-13 12:12:12 +0100

    adds more content to runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -5,26 +5,46 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        Carnivore raptor = new Carnivore(Species.RAPTOR, true);
-        DBHelper.save(raptor);
-        Carnivore allosaurus = new Carnivore(Species.ALLOSAURUS, true);
-        DBHelper.save(allosaurus);
-        Herbivore minmi = new Herbivore(Species.MINMI, true);
-        DBHelper.save(minmi);
-        Herbivore steg = new Herbivore(Species.STEGOSAURUS, true);
-        DBHelper.save(steg);
-
-        Paddock paddock1 = new Paddock(1);
-        DBHelper.save(paddock1);
-
-        Paddock paddock2 = new Paddock(2);
-        DBHelper.save(paddock2);
-
         Visitor visitor1 = new Visitor("Colin");
-        DBHelper.save(visitor1);
+
 
         Visitor visitor2 = new Visitor("Tara");
+
+
+        Carnivore raptor = new Carnivore(Species.RAPTOR, true);
+
+        Herbivore minmi = new Herbivore(Species.MINMI, true);
+
+        Herbivore steg = new Herbivore(Species.STEGOSAURUS, true);
+
+
+        Paddock paddock1 = new Paddock(1);
+
+
+        Paddock paddock2 = new Paddock(2);
+
+
+        Park park = new Park("Jurassic Park");
+
+        paddock1.addDinosaur(raptor);
+        paddock2.addDinosaur(minmi);
+        paddock2.addDinosaur(steg);
+
+        park.addPaddock(paddock1);
+        park.addPaddock(paddock2);
+
+        park.addVisitor(visitor1);
+        park.addVisitor(visitor2);
+
+        DBHelper.save(visitor1);
         DBHelper.save(visitor2);
+        DBHelper.save(paddock1);
+        DBHelper.save(paddock2);
+        DBHelper.save(raptor);
+        DBHelper.save(minmi);
+        DBHelper.save(steg);
+        DBHelper.save(park);
+
 
     }
 
