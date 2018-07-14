c84d26ab8a4ef6d6d97ac291bf116912c1c6ba0f
Roddy Daly <roddy_daly110@hotmail.com>
2018-07-14 12:16:38 +0100

    added parks controller and layout.vtl"

--- src/main/java/controllers/ParkController.java
+++ src/main/java/controllers/ParkController.java
@@ -1,4 +1,33 @@
 package controllers;
 
+import db.DBHelper;
+import models.Park;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.*;
+
+
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
 public class ParkController {
+
+    public static void main(String[] args) {
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+        staticFileLocation("/resources");
+        Seeds.seedData();
+
+
+        List<Park> parks = DBHelper.getAll(Park.class);
+
+
+        get ("/", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("parks", parks);
+            model.put("template", "park/index.vtl");
+            return new ModelAndView(model, "layout.vtl");
+        }, velocityTemplateEngine);
+
+    }
 }
