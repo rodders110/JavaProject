a6339d4167127f247175f8f614b96ef3332fa395
Roddy <roddy_daly110@hotmail.com>
2018-07-17 12:29:18 +0100

    can now add paddocks from paddocks view

--- src/main/java/controllers/PaddockController.java
+++ src/main/java/controllers/PaddockController.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class PaddockController {
 
@@ -36,6 +37,17 @@ public class PaddockController {
             model.put("parks", parks);
             return new ModelAndView(model, "layout.vtl");
         }, new VelocityTemplateEngine());
+
+        post("/paddock/paddock/new", (req, res) -> {
+            int parkId = Integer.parseInt(req.queryParams("park_id"));
+            int newPadNum = Integer.parseInt(req.queryParams("pad_num"));
+            Park park = DBHelper.find(Park.class, parkId);
+            Paddock newPad = new Paddock(newPadNum);
+            newPad.setPark(park);
+            DBHelper.save(newPad);
+            res.redirect("/paddocks");
+            return null;
+        },new VelocityTemplateEngine());
 
     }
 }
